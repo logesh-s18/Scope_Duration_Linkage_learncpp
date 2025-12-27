e093ad1d22f38787a30b23b6958abada86f005f9
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2025-12-27 16:23:59 +0530

    Variable scope - basics

--- main.cpp
+++ main.cpp
@@ -13,9 +13,32 @@ int main()
 {
 	// Namespace aliases
 
-	namespace demo = Logesh::old;
+	//namespace demo = Logesh::old;
 
-	demo::printMyyName_L_o();
+	//demo::printMyyName_L_o();
 
+	int x{};
+
+	x = 12;
+
+	int z{};
+
+	{
+	
+		int y{};
+
+
+		int x{};
+
+		x = 20;
+		x = 1;
+
+
+	}
+
+	int j{};
+
+
+	return 0;
 
 }
